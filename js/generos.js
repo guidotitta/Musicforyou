b07a85ea79bae311c16a285b440d2e1047aa8fbd
guidotitta <gtittarelli@udesa.edu.ar>
2020-06-17 15:57:53 -0300

    hice el js de generos

---
+++ js/generos.js
@@ -0,0 +1,25 @@
+fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/')
+.then(function(response){
+    return response.json()
+})
+.then(function(datos){
+    console.log(datos);
+    let generos= datos.data 
+    let contenedorgeneros=document.querySelector('#genero')
+    for (const genero of generos) {
+        contenedorgeneros.innerHTML+=`
+        <div class="producto">
+                    <h3> ${genero.name} </h3>
+                    <a href="detalleAlbum.html"><img src="${genero.picture_big}" alt=""></a> 
+                </div>
+        
+        
+        
+        
+        `
+        
+    }
+    
+    
+      
+})
