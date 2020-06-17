dbb257affbd542e71a369d88ba5dcdfe4d486ebb
FGaibisso <fgaibissobono@udesa.edu.ar>
2020-06-17 16:13:50 -0300

    agregado detalleTrack.js

---
+++ js/detalleTrack.js
@@ -0,0 +1,32 @@
+let queryString = location.search
+let queryStringObj = new URLSearchParams(queryString)
+let trackId = queryStringObj.get('id')
+
+fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/' + trackId)
+.then(function(response){
+    return response.json()
+})
+.then(function(datos){
+    console.log(datos);
+
+    let contento = document.querySelector('#contento')
+
+    contento.innerHTML = `
+    <div id='main'>
+    <h2>
+        DETALLE TRACK
+    </h2> 
+    <div id='track'>
+        <div><img class='header_track' src="${datos.album.cover_big}" alt=""></div>
+        <div class='track1'>
+        <div class='header_track2'>
+    <h3>${datos.title}</h3>
+    <p> <a href="playlist.html">Agregar a playlist</a></p>
+    </div>          
+        <p><a href="detalleAlbum.html">Album: ${datos.album.title}</a></p>
+        <p><a href="listadodegeneros.html">Rank: ${datos.rank}</a></p>
+        <p>Fecha de Lanzamiento: ${datos.release_date}</p>
+</div>
+</div>
+</div>`
+})
