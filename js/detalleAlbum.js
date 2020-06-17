9110972187fe92fc555b0c4224e932fa5d4483dd
tomicampastro <tomycampastrogallo@gmail.com>
2020-06-17 16:04:45 -0300

    El js de detalleAlbum

---
+++ js/detalleAlbum.js
@@ -0,0 +1,32 @@
+let querystring = location.search
+let querystringobj = new URLSearchParams (querystring)
+let albumid = querystringobj.get ("id")
+
+fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/'+ albumid)
+.then(function(response){
+    return response.json()
+})
+.then(function(datos){
+    console.log(datos);
+    let contento = document.querySelector ("#contento")
+    contento.innerHTML =
+     `<div id='main'>
+                <h2>
+                    DETALLE ALBUM
+                </h2> 
+                <div id='track'>
+                    <div><img class='header_track' src="${datos.cover_big}" alt="nombre album"></div>
+                    <div class='track1'>
+                        <h3>Titulo: ${datos.title}</h3>          
+                            <p><a href="detalleArtista.html">Artista: ${datos.artist.name}</a></p>
+                            <p><a href="listadodegeneros.html">Genero: ${datos.genres.data[0].name}</a></p>
+                            <p>Discografica: ${datos.label}</p>
+                            <p>Fecha de Lanzamiento: ${datos.release_date}</p>
+                    </div>
+                </div>
+            </div>`
+    
+})
+
+
+
