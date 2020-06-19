b9256fab2c653c854bc4bacb473119392ee06080
guidotitta <gtittarelli@udesa.edu.ar>
2020-06-18 21:29:51 -0300

    js mejorado

--- js/detalleAlbum.js
+++ js/detalleAlbum.js
@@ -18,8 +18,8 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/'+ album
                     <div><img class='header_track' src="${datos.cover_big}" alt="nombre album"></div>
                     <div class='track1'>
                         <h3>Titulo: ${datos.title}</h3>          
-                            <p><a href="detalleArtista.html">Artista: ${datos.artist.name}</a></p>
-                            <p><a href="listadodegeneros.html">Genero: ${datos.genres.data[0].name}</a></p>
+                            <p><a href="detalleArtista.html?id=${datos.id}">Artista: ${datos.artist.name}</a></p>
+                            <p><a href="listadodegeneros.html?id=${datos.id}">Genero: ${datos.genres.data[0].name}</a></p>
                             <p>Discografica: ${datos.label}</p>
                             <p>Fecha de Lanzamiento: ${datos.release_date}</p>
                     </div>
