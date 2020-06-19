ec3435a67b06172f288a9ab1ff4566b7b6fce788
tomicampastro <tomycampastrogallo@gmail.com>
2020-06-18 22:21:34 -0300

    js de playlist

---
+++ js/playlist.js
@@ -0,0 +1,15 @@
+fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/playlists')
+.then(function(response){
+    return response.json()
+})
+.then(function(datos){
+    console.log(datos);
+    let conteiner = document.querySelector("#genero")
+    for (const playlist of datos.data) {
+        conteiner.innerHTML += `<div class="producto">
+        <a href="detalleTrack.html"><img src="${playlist.picture_big}" alt=""></a> 
+        <h3>Duki - Rockstar</h3>
+    </div>`
+        
+    }
+})
